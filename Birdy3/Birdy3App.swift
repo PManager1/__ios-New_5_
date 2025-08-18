73c26258979b5b839fabb9436112e9233e16c437
Jay <jpca999@gmail.com>
2025-08-18 01:05:19 -0400

    added mutliple file sincluein gitnore

---
+++ Birdy3/Birdy3App.swift
@@ -0,0 +1,17 @@
+//
+//  Birdy3App.swift
+//  Birdy3
+//
+//  Created by jay P on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Birdy3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
